c15c45d48cf6c2a158fe47715a55ef6a0864f45a
Tom Holohan <thomasholohan8@gmail.com>
2021-10-01 14:44:29 +0000

    Create static and templates directories

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Appointment(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    date = models.DateField()
+    time = models.TimeField()
+    message = models.CharField(max_length=500)
